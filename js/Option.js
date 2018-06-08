47a2c372633d1cb267c3d44fc0542f61c935df0d
Naisho <antoinedugue@sfr.fr>
2018-06-08 10:58:50 +0200

    OPtions js
    Antoine

--- js/Option.js
+++ js/Option.js
@@ -1,11 +1,22 @@
+$(".singleMessage").click(function () {
+    // switch activation / désactivation div .messageOption
+    if
+        // afficher la div
 
-$( ".copy").click(function() {
-    $( "" ).clone();
-});
 
-$( ".translate").click(function() {
-    $( "" ).select();
-});
-$( ".correct").click(function() {
-    $( "" ).select();
-});
+        // fonction copie
+    $( ".copy").click(function() {
+        $( "" ).clone();
+    });
+        // api google translate
+    $( ".translate").click(function() {
+        $( "" ).select();
+    });
+    // fonction correction, récupère le message, en fait une copie dans un champs non modifiable et une dans un champs texte
+    $( ".correct").click(function() {
+        $( "" ).select();
+    });
+
+    // si la div est déjà affichée, la cachée
+})
+
